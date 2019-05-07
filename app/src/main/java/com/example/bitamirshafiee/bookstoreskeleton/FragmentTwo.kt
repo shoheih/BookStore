ee8a27514f2917ad1c6a59e8034e0811918b480e
shoheih <shoheigithub@gmail.com>
2019-05-08 05:36:58 +0900

    RecyclerView with a GridLayoutManager in Fragment Two

--- app/src/main/java/com/example/bitamirshafiee/bookstoreskeleton/FragmentTwo.kt
+++ app/src/main/java/com/example/bitamirshafiee/bookstoreskeleton/FragmentTwo.kt
@@ -3,21 +3,41 @@ package com.example.bitamirshafiee.bookstoreskeleton
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class FragmentTwo : Fragment() {
+
+    private var childrenList: MutableList<Int> = mutableListOf(
+        R.drawable.children_one,
+        R.drawable.children_two,
+        R.drawable.children_three,
+        R.drawable.children_four,
+        R.drawable.children_five,
+        R.drawable.children_six,
+        R.drawable.children_seven,
+        R.drawable.children_eight,
+        R.drawable.children_nine,
+        R.drawable.children_ten,
+        R.drawable.children_one,
+        R.drawable.children_two,
+        R.drawable.children_three,
+        R.drawable.children_four,
+        R.drawable.children_five,
+        R.drawable.children_six,
+        R.drawable.children_seven,
+        R.drawable.children_eight,
+        R.drawable.children_nine,
+        R.drawable.children_ten
+    )
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: RecyclerViewAdapter
+    private lateinit var layout: GridLayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,7 +46,14 @@ class FragmentTwo : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_two, container, false)
 
+        recyclerView = view.findViewById(R.id.recycler_view)
 
+        adapter = RecyclerViewAdapter(childrenList)
+
+        layout = GridLayoutManager(context!!, 3)
+
+        recyclerView.layoutManager = layout
+        recyclerView.adapter = adapter
 
         return view
     }
