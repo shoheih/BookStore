13483e1120c9514b868fc0977a111c6e587fc1e6
shoheih <shoheigithub@gmail.com>
2019-05-07 13:38:41 +0900

    Using a GridView in Fragment One

--- app/src/main/java/com/example/bitamirshafiee/bookstoreskeleton/FragmentOne.kt
+++ app/src/main/java/com/example/bitamirshafiee/bookstoreskeleton/FragmentOne.kt
@@ -6,9 +6,35 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridView
 
 
 class FragmentOne : Fragment() {
+
+    private lateinit var gridView: GridView
+
+    var topList: MutableList<Int> = mutableListOf(
+        R.drawable.top_one,
+        R.drawable.top_two,
+        R.drawable.top_three,
+        R.drawable.top_four,
+        R.drawable.top_five,
+        R.drawable.top_six,
+        R.drawable.top_seven,
+        R.drawable.top_eight,
+        R.drawable.top_nine,
+        R.drawable.top_ten,
+        R.drawable.top_one,
+        R.drawable.top_two,
+        R.drawable.top_three,
+        R.drawable.top_four,
+        R.drawable.top_five,
+        R.drawable.top_six,
+        R.drawable.top_seven,
+        R.drawable.top_eight,
+        R.drawable.top_nine,
+        R.drawable.top_ten
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,7 +43,11 @@ class FragmentOne : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_one, container, false)
 
+        gridView = view.findViewById(R.id.grid_view)
 
+        val adapter = TopListAdapter(topList, context!!)
+
+        gridView.adapter = adapter
 
         return view
     }
---
+++ app/src/main/java/com/example/bitamirshafiee/bookstoreskeleton/TopListAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.bitamirshafiee.bookstoreskeleton
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+
+class TopListAdapter(private var topList: MutableList<Int>, private var context: Context): BaseAdapter() {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+        var imageView: ImageView
+
+        if (convertView == null) {
+            imageView = ImageView(context)
+            imageView.layoutParams = ViewGroup.LayoutParams(240, 400)
+            imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+            imageView.setPadding(8, 8, 8,8)
+        } else {
+            imageView = convertView as ImageView
+        }
+        imageView.setImageResource(topList[position])
+        return imageView
+    }
+
+    override fun getItem(position: Int): Any? = null
+
+    override fun getItemId(position: Int): Long = 0L
+
+    override fun getCount(): Int {
+        return topList.size
+    }
+}
