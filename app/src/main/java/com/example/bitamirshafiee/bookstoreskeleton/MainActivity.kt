e5bbe8f5b1c8ad492dae45de369684c7dca0ba07
shoheih <shoheigithub@gmail.com>
2019-05-09 06:44:35 +0900

    Adding NavigationDrawer

--- app/src/main/java/com/example/bitamirshafiee/bookstoreskeleton/MainActivity.kt
+++ app/src/main/java/com/example/bitamirshafiee/bookstoreskeleton/MainActivity.kt
@@ -1,18 +1,52 @@
 package com.example.bitamirshafiee.bookstoreskeleton
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
 import android.support.design.widget.TabLayout
+import android.support.v4.view.GravityCompat
 import android.support.v4.view.ViewPager
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBar
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewPager: ViewPager
     private lateinit var tabLayout: TabLayout
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val actionBar: ActionBar? = supportActionBar
+
+        actionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
+
+        val navigationView: NavigationView = findViewById(R.id.navigation_view)
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.profile -> {
+
+                }
+                R.id.about -> {
+                    val intent = Intent(this@MainActivity, AboutActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+
+            drawerLayout.closeDrawers()
+            true
+        }
 
         viewPager = findViewById(R.id.view_pager)
         tabLayout = findViewById(R.id.tab_layout)
@@ -22,5 +56,16 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = pagerAdapter
 
         tabLayout.setupWithViewPager(viewPager)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when(item?.itemId) {
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+
     }
 }
